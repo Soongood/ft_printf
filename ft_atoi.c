ddf42b0d06170c703331c6f49d53b9e48031c961
Soongood <46536006+Soongood@users.noreply.github.com>
2020-01-16 21:40:24 +0300

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trobbin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/20 00:27:36 by trobbin           #+#    #+#             */
+/*   Updated: 2019/09/21 17:52:12 by trobbin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int		sign;
+	long	result;
+
+	sign = 1;
+	result = 0;
+	while (*str == ' ' || *str == '\t' || *str == '\n'
+			|| *str == '\f' || *str == '\r' || *str == '\v')
+		str++;
+	if (*str == '+')
+		str++;
+	else if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	while (ft_isdigit(*str))
+	{
+		result = 10 * result + (*str - '0');
+		str++;
+	}
+	return (sign * result);
+}
--- ft_printf.c
+++ ft_printf.c
@@ -1,5 +1,7 @@
 #include "ft_printf.h"
+#include <unistd.h>
 #include <stdarg.h>
+#include <inttypes.h>
 #include <stdio.h>
 
 
@@ -15,100 +17,136 @@ You must manage the minimum field-width
 You must manage the precision
 */
 
-/* typedef struct			s_struct
+typedef struct			s_str
 {
-	struct	s_struct	*next;
-	int					precision;
-	int					f_width;
-	char				*flags;
-	char				*length;
-	char				type;
-	char				str_out;
-}						t_struct;
-*/
+	char				flags[5];
+	char				width[5];
+	char				precision[5];
+	char				size[3];
+	char				type[2];
+}						t_str;
 
-int		ft_parse_flags(char **chunk)
+
+void	parse_flags(char *chunk, char *flags, int *shift)
 {
-	int shift;
-
-	shift = 0;
-	while (**chunk == SPACE || **chunk == SHARP || **chunk == PLUS || **chunk == MINUS || **chunk == ZERO)
+	while (*chunk == ' ' || *chunk == '#' || *chunk == '+' || *chunk == '-' || *chunk == '0')
 	{
-		**chunk++;
-		shift++;
+		*flags++ = *chunk++;
+		*shift += 1;
 	}
-	return (shift);
 }
-int		ft_parse_width(char *chunk)
-{
-	int shift;
 
-	shift = 0;
+void	parse_width(char *chunk, char *width, int *shift)
+{
 	while (ft_isdigit(*chunk))
 	{
-		chunk++;
-		shift++;
+		*width++ = *chunk++;
+		*shift += 1;
 	}
-	return (shift);
 }
-int		ft_parse_precision(char *chunk)
-{
-	int shift;
 
-	shift = 0;
+void	parse_precision(char *chunk, char *precision, int *shift)
+{
 	if (*chunk++ == '.')
 	{
 		while (ft_isdigit(*chunk))
 		{
-			chunk++;
-			shift++;
+			*precision++ = chunk++;
+			*shift += 1;
+		}
+		*shift += 1;
+	}
+}
+
+int		is_type(char *chunk)
+{
+	return (*chunk == 'p' || *chunk == 'c' || *chunk == 's');
+}
+
+int		is_type_com(char *chunk)
+{
+	return (*chunk == 'd' || *chunk == 'i' || *chunk == 'o' || *chunk == 'u' || *chunk == 'x' || *chunk == 'X');
+}
+
+int		is_type_f(char *chunk)
+{
+	return (*chunk == 'f');
+}
+
+void	parse_s(t_str line, char *chunk, int *shift, va_list *list)
+{
+	int i;
+
+	i = 0;
+	if (is_type(chunk[i]) || is_type_com(chunk[i]) || is_type_f(chunk[i++]))
+		handle();
+	else 
+	{
+		while (chunk[i] && i < 3)
+		{
+			if (i == 1 && chunk[0] == 'l' && (is_type_f(chunk[i]) || is_type_com(chunk[i])))
+			{
+				handle();
+				i++;
+				break;
+			}
+			if (i == 1 && chunk[0] == 'L' && is_type_f(chunk[i]))
+			{
+				handle();
+				i++;
+				break;
+			}
+			else if (chunk[0] == chunk[1])
 		}
 	}
+	*shift += i;
+}
+
+int		parse(char *chunk, t_str line, va_list *list)
+{
+	int shift;
+	
+	shift = 0;
+	parse_flags(chunk, line.flags, &shift);
+	parse_width(chunk + shift, line.width, &shift);
+	parse_precision(chunk + shift, line.precision, &shift);
+	parse_s(line, chunk + shift, &shift, list);
 	return (shift);
 }
 
- int		ft_parse_type(char *chunk)
+int	ft_printf(const char *format, ...)
 {
-	return (1);
-}
+	int		size;
+	char	*tmp;
+	t_str	line;
+	va_list	ap;
 
-int		ft_parse_number(char **chunk)
-{
-	static int shift = 0;
-	
-	shift += ft_parse_flags(chunk);
-	shift += ft_parse_width(chunk + shift);
-	shift += ft_parse_precision(chunk + shift);
-	shift += ft_parse_type(chunk + shift);
-	return (shift);
-}
-
-void	ft_parse_line(const char *format)
-{
-	char **tmp;
-	
+	size = 0;
+	tmp = format;
+	va_start(ap, format);
 	while (*format)
 	{
-		if (*format != '%')
-			format++;
+		if (*format++ != '%')
+			size++;
 		else
 		{
-			format += 1;
-			tmp = &format;
-			format += ft_parse_number(tmp);
+			write(1, tmp, size);
+			size = 0;
+			ft_bzero(&line, sizeof(line));
+			format += parse(format, line, &ap);
+			tmp = format;
 		}
 	}
-}
-
-int		ft_printf(const char *format, ...)
-{
-	ft_parse_line(format);
+	va_end(ap);
+	write(1, tmp, size);
+	return (0);
 }
 
 int		main()
 {
-	int number = 1231425;
-	ft_printf("sdkfjf %+-123.3i");
-	//printf(" %45 \n", number); 
-return (0);
+	int number = 120;
+
+	ft_printf("%c", number);
+//	printf(" %zz%zz %i\n", number); 
+	return (0);
 }
