bb08922b929df45b9824f34a3bb1b85c8f6231ae
Soongood <46536006+Soongood@users.noreply.github.com>
2020-01-21 00:59:22 +0300

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -19,98 +19,137 @@ You must manage the precision
 
 typedef struct			s_str
 {
-	char				flags[5];
-	char				width[5];
-	char				precision[5];
-	char				size[3];
-	char				type[2];
-}						t_str;
+	uint8_t				flags;
+	int					width;
+	int					precision;
+	char				specifier[2];
+	char				type[1];
+}						t_str; // не забыть выровнять
 
-
-void	parse_flags(char *chunk, char *flags, int *shift)
-{
-	while (*chunk == ' ' || *chunk == '#' || *chunk == '+' || *chunk == '-' || *chunk == '0')
-	{
-		*flags++ = *chunk++;
-		*shift += 1;
-	}
-}
-
-void	parse_width(char *chunk, char *width, int *shift)
-{
-	while (ft_isdigit(*chunk))
-	{
-		*width++ = *chunk++;
-		*shift += 1;
-	}
-}
-
-void	parse_precision(char *chunk, char *precision, int *shift)
-{
-	if (*chunk++ == '.')
-	{
-		while (ft_isdigit(*chunk))
-		{
-			*precision++ = chunk++;
-			*shift += 1;
-		}
-		*shift += 1;
-	}
-}
-
-int		is_type(char *chunk)
-{
-	return (*chunk == 'p' || *chunk == 'c' || *chunk == 's');
-}
-
-int		is_type_com(char *chunk)
-{
-	return (*chunk == 'd' || *chunk == 'i' || *chunk == 'o' || *chunk == 'u' || *chunk == 'x' || *chunk == 'X');
-}
-
-int		is_type_f(char *chunk)
-{
-	return (*chunk == 'f');
-}
-
-void	parse_s(t_str line, char *chunk, int *shift, va_list *list)
+int		is_flags(t_str *line, char *chunk)
 {
 	int i;
 
 	i = 0;
-	if (is_type(chunk[i]) || is_type_com(chunk[i]) || is_type_f(chunk[i++]))
-		handle();
-	else 
+	while (chunk[i] == ' ' || chunk[i] == '#' || chunk[i] == '+' || chunk[i] == '-' || chunk[i] == '0')
 	{
-		while (chunk[i] && i < 3)
-		{
-			if (i == 1 && chunk[0] == 'l' && (is_type_f(chunk[i]) || is_type_com(chunk[i])))
-			{
-				handle();
-				i++;
-				break;
-			}
-			if (i == 1 && chunk[0] == 'L' && is_type_f(chunk[i]))
-			{
-				handle();
-				i++;
-				break;
-			}
-			else if (chunk[0] == chunk[1])
-		}
+		if (chunk[i] == '-')
+			line->flags |= MINUS;
+		if (chunk[i] == '+')
+			line->flags |= PLUS;
+		if (chunk[i] != ' ')
+			line->flags |= SPACE;
+		if (chunk[i] != '0')
+			line->flags |= ZERO;
+		if (chunk[i] != '#')
+			line->flags |= SHARP;
+		i++;
 	}
-	*shift += i;
+	return (i);
 }
 
-int		parse(char *chunk, t_str line, va_list *list)
+void	parser_one(char *chunk, t_str *line, int *shift)
+{
+	int		i;
+	int		j;
+	char	tmp[5];
+
+	i = 0;
+	j = 0;
+	ft_bzero(&tmp, sizeof(tmp));
+	j = is_flags(line, chunk);
+	i = 0;
+	while (ft_isdigit(chunk[j]))
+		tmp[i++] = chunk[j++];
+	*tmp ? line->width = ft_atoi(tmp), ft_bzero(&tmp, i) : 0;
+	i = 0;
+	if (chunk[j] == '.')
+		while (ft_isdigit(chunk[++j]))
+			tmp[i++] = chunk[j];
+	line->precision = ft_atoi(tmp);
+	*shift += j;
+}
+
+int		is_type(char chunk)
+{
+	return (chunk == 'p' || chunk == 'c' || chunk == 's' ||
+			chunk == 'd' || chunk == 'i' || chunk == 'o' ||
+			chunk == 'u' || chunk == 'x' || chunk == 'X' ||
+			chunk == 'f');
+}
+
+int		is_specifier(char chunk)
+{
+	return (chunk == 'l' || chunk == 'h');
+}
+
+int		parser_sec(t_str *line, char *chunk, int *shift)
+{
+	int i;
+
+	i = 0;
+	if (is_type(chunk[i++]))
+		line->type[0] = chunk[0];
+	else if (chunk[i++])
+		{
+			if (is_specifier(chunk[0]) && (chunk[0] == chunk[1]) && is_type(chunk[i]))
+				line->specifier[1] = (chunk[1]);
+			else if ((is_specifier(chunk[0]) || chunk[0] == 'L') && is_type(chunk[i - 1]))
+				i--;
+			else
+				return (1);
+			line->specifier[0] = chunk[0];
+			line->type[0] = chunk[i++];
+		}
+		else
+			return (1);
+	*shift += i;
+	return (0);
+}
+
+void	c_type(t_str line, va_list list)
+{
+	char	letter;
+
+	letter = va_arg(list, int);
+	if (line.width)
+		if(line.flags == (line.flags | MINUS))
+			write(1, &letter, 1);
+	while (--line.width > 0)
+		write(1, " ", 1);
+	if(line.flags == (line.flags | MINUS))
+		return ;
+	write(1, &letter, 1);
+}
+
+void	handler(t_str line, va_list list)
+{
+	if (*line.type == 'c')
+		c_type(line, list);
+	// if (*line.type == 'd' || *line.type == 'i')
+	// 	*shift = d_type(line, list);
+	// if (*line.type == 's')
+	// 	*shift = s_type(line, list);
+	// if (*line.type == 'o')
+	// 	*shift = o_type(line, list);
+	// if (*line.type == 'f')
+	// 	*shift = f_type(line, list);
+	// if (*line.type == 'u')
+	// 	*shift = u_type(line, list);
+	// if (*line.type == 'p')
+	// 	*shift = p_type(line, list);
+	// if (*line.type == 'x' || *line.type == 'X')
+	// 	*shift = x_type(line, list);
+}
+
+int		parsing(char *chunk, t_str line, va_list list)
 {
 	int shift;
 	
 	shift = 0;
-	parse_flags(chunk, line.flags, &shift);
-	parse_width(chunk + shift, line.width, &shift);
-	parse_precision(chunk + shift, line.precision, &shift);
-	parse_s(line, chunk + shift, &shift, list);
+	parser_one(chunk, &line, &shift);
+	if (!(parser_sec(&line, chunk + shift, &shift)))
+		handler(line, list);
 	return (shift);
 }
 
@@ -123,20 +162,20 @@ int	ft_printf(const char *format, ...)
 
 	size = 0;
 	tmp = format;
+	ft_bzero(&line, sizeof(line));
 	va_start(ap, format);
 	while (*format)
-	{
 		if (*format++ != '%')
 			size++;
+		else if (*format == '%' && tmp++ && format++)
+				size++;
 		else
 		{
 			write(1, tmp, size);
 			size = 0;
-			ft_bzero(&line, sizeof(line));
-			format += parse(format, line, &ap);
+			format += parsing(format, line, ap);
 			tmp = format;
 		}
-	}
 	va_end(ap);
 	write(1, tmp, size);
 	return (0);
@@ -144,9 +183,10 @@ int	ft_printf(const char *format, ...)
 
 int		main()
 {
-	int number = 120;
+	char number = 348;
 
-	ft_printf("%c", number);
-//	printf(" %zz%zz %i\n", number); 
+	ft_printf("%-4cf\n", number);
+	printf("%4cf\n", number);
+	printf("%-4cf\n", number); 
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -1,7 +1,14 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int	ft_isdigit(int c);
+# define MINUS 1
+# define PLUS 2
+# define SPACE 4
+# define SHARP 8
+# define ZERO 16
+
+#include "libft.h"
+
 int ft_printf(const char *format, ...);
 
 #endif
